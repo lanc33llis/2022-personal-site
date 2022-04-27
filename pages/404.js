eb155b5fc26eb5f3ca3e47f2ad793df14a02684f
Lance Ellis <scdelance@gmail.com>
2022-04-27 07:22:44 -0500

    404 page

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,16 @@
+import Layout from './components/Layout'
+
+import styles from '../styles/404.module.sass'
+
 export default function NotFound() {
   return (
-    <h1>
-      Not Found
-    </h1>
+    <Layout>
+      <div className={styles.container}>
+        <h1>404</h1>
+        <p>
+          Sorry, the page you are looking for does not exist.
+        </p>
+      </div>
+    </Layout>
   )
 }
