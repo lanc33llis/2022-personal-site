d437379a25ec3269eb1e76dc7e2590d10a4ad08f
Lance Ellis <scdelance@gmail.com>
2022-04-26 23:45:40 -0500

    var and coloring change

--- pages/components/Footer.jsx
+++ pages/components/Footer.jsx
@@ -1,7 +1,25 @@
-const Footer = () => (
-  <footer>
+import styles from '../../styles/Footer.module.sass'
 
-  </footer>
+import Image from 'next/image'
+import Link from 'next/link'
+
+const Footer = () => (
+  <div className={styles.container}>
+    <footer>
+      <div className={styles.links}>
+        <Link href="https://github.com/lanc33llis" passHref>
+          <a>
+            <Image src="/Github.svg" alt="Github" width={28} height={28} />
+          </a>
+        </Link>
+        <Link href="https://www.linkedin.com/in/lance-ellis" passHref>
+          <a>
+            <Image src="/whitelinkedin.svg" width={28} height={28} alt="Linkedin" />
+          </a>
+        </Link>
+      </div>
+    </footer>
+  </div>
 )
 
 export default Footer
