508fd450ecfd5b4b23e8230693b2696fcd28efcd
Lance Ellis <scdelance@gmail.com>
2022-04-28 03:32:16 -0500

    fixes nav bug finally

--- pages/components/Header.jsx
+++ pages/components/Header.jsx
@@ -6,6 +6,8 @@ import { useRouter } from 'next/router'
 
 import { LayoutGroup, motion } from 'framer-motion'
 
+import { memo } from 'react'
+
 const links = [
   { name: 'Home', href: '/' },
   { name: 'Blog', href: ['/blog', '/blogs'] },
@@ -13,44 +15,48 @@ const links = [
   { name: 'Contact', href: '/contact' },
 ]
 
+const Nav = memo(({ router }) => (
+  // <LayoutGroup>
+    <motion.nav layout>
+      {links.map(({ name, href }) => (
+        <Link href={Array.isArray(href) ? href[0] : href} key={name} passHref>
+          <a className={styles.link}>
+            {
+              ((Array.isArray(href) && href.includes(`/${router.pathname.split('/')[1]}`)) || (router.pathname === href)) &&
+              <motion.span
+                layoutId="nav-selected"
+                className={styles.active}
+                animate={{x: 0}}
+              />
+            }
+            {name}
+          </a>
+        </Link>
+      ))}
+    </motion.nav>
+  // </LayoutGroup>
+))
+Nav.displayName = 'Nav'
+
 const Header = () => {
   const router = useRouter()
 
   return (
-    <LayoutGroup>
-      <div className={styles.container}>
-        <header>
-          <span>
-            Lance Ellis
-          </span>
-          <nav>
-            {links.map(({ name, href }) => (
-              <Link href={Array.isArray(href) ? href[0] : href} key={name} passHref>
-                <a className={styles.link}>
-                  {
-                    ((Array.isArray(href) && href.includes(`/${router.pathname.split('/')[1]}`)) || (router.pathname === href)) &&
-                    <motion.span
-                      layoutId="nav-selected"
-                      className={styles.active}
-                      style={{top: "24.75px", width: '100%', left: '-.9em'}}
-                      animate
-                    />
-                  }
-                  {name}
-                </a>
-              </Link>
-            ))}
-          </nav>
-          <span style={{paddingTop: "4px"}}>
-            <Link href="https://github.com/lanc33llis" passHref>
-              <a className={styles.github}>
-                <Image src="/Github.svg" alt="Github" width={32} height={32} />
-              </a>
-            </Link>
-          </span>
-        </header>
-      </div>
-    </LayoutGroup>
+    <div className={styles.container}>
+      <header>
+        <span>
+          Lance Ellis
+        </span>
+        <Nav router={router}/>
+        <span style={{paddingTop: "4px"}}>
+          <Link href="https://github.com/lanc33llis" passHref>
+            <a className={styles.github}>
+              <Image src="/Github.svg" alt="Github" width={32} height={32} />
+            </a>
+          </Link>
+        </span>
+      </header>
+    </div>
   )
 }
 
